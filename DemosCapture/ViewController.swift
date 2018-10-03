9e9b70c9b237b8dee5cf2701c17ba551d335f50d
Christophe Milliere <c.milliere@bazarchic.com>
2018-10-03 09:16:58 +0200

    Initial Commit

---
+++ DemosCapture/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DemosCapture
+//
+//  Created by Christophe on 03/10/2018.
+//  Copyright © 2018 Christophe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
