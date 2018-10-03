f86869f78bb62f82fe14f95221d323f168fd5811
Christophe Milliere <c.milliere@bazarchic.com>
2018-10-03 14:03:44 +0200

    finish

--- DemosCapture/ViewController.swift
+++ DemosCapture/ViewController.swift
@@ -8,16 +8,61 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+import AVFoundation
 
+class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
+    
+    @IBOutlet weak var ui_startSession: UIButton!
+    @IBOutlet weak var ui_preview: UIView!
+    let captureSession = AVCaptureSession()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func startSessionButtonPressed(_ sender: Any) {
+        if captureSession.isRunning {
+            captureSession.stopRunning()
+            ui_startSession.titleLabel?.text = "Démarrer la capture"
+        } else {
+            if captureSession.inputs.count == 0 {
+                configureCaptureSession()
+                ui_startSession.titleLabel?.text = "Arréter la capture"
+            }
+            // 4 - Démarrer la session
+            captureSession.startRunning()
+            ui_startSession.titleLabel?.text = "Arréter la capture"
+        }
+    }
+    
+    func configureCaptureSession() {
+        // 1 - Configurer les entrées
+        if let camera = AVCaptureDevice.default(for: AVMediaType.video),
+            let cameraFeed = try? AVCaptureDeviceInput(device: camera) {
+            captureSession.addInput(cameraFeed)
+            
+            // 2 - Configurer les sorties
+            let outputFeed = AVCaptureVideoDataOutput()
+            outputFeed.setSampleBufferDelegate(self, queue: DispatchQueue.global(qos: DispatchQoS.QoSClass.userInitiated))
+            captureSession.addOutput(outputFeed)
+            
+            // 3 - Configurer l'aperçu
+            let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+            previewLayer.frame = ui_preview.bounds
+            ui_preview.layer.addSublayer(previewLayer)
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        guard let pixelbufer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
+    }
+
+    func captureOutput(_ output: AVCaptureOutput, didDrop sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
     }
 
 
